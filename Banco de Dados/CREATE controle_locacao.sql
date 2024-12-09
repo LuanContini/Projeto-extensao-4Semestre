9f1b988a440c56ff87e6a1a1bc4d3753314e2491
Antônio Matheus <74572968+LikeTony@users.noreply.github.com>
2024-12-09 19:24:32 -0300

    Ajustando alguns problemas de syntax

--- Banco de Dados/CREATE controle_locacao.sql
+++ Banco de Dados/CREATE controle_locacao.sql
@@ -7,11 +7,11 @@ CREATE TABLE IF NOT EXISTS usuario (
 	nome VARCHAR(100) NOT NULL,
 	cpf VARCHAR(11) UNIQUE NOT NULL, -- Validar tamanho do CPF como 11 digitos
 	email VARCHAR(100) NOT NULL, -- Email deve conter um validador de email
-	senha VARCHAR(50) NOT NULL, -- Armazenar com criptografia + salt
+	senha VARCHAR(100) NOT NULL, -- Armazenar com criptografia + salt
 	dataNasc DATE, -- Validar idade minima ou datas posteriores a atual e se ajustar ao padrao (YYYY-MM-DD)
-	telefone INT NOT NULL,
+	telefone VARCHAR(20) NOT NULL,
 	data_cad TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	tipo ENUM('Administrador', 'Operador') NOT NULL DEFAULT ('Operador'), -- Tipo so pode ser armazenado com 1 desses 2 tipos
+	tipo ENUM('Administrador', 'Operador') NOT NULL DEFAULT 'Operador', -- Tipo so pode ser armazenado com 1 desses 2 tipos
 	imagem LONGBLOB,
 	PRIMARY KEY(idUsuario),
     CHECK(LENGTH(nome) >= 3), -- Validar tamanho do nome é maior ou igual que 3 caracteres
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS contratante (
     idContratante INT AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL, -- Email deve conter um validador de email
-    telefone INT NOT NULL, -- Validar minimo de números
+    telefone VARCHAR(20) NOT NULL, -- Validar minimo de números
     imagem LONGBLOB,
     observacao VARCHAR(300),
 	CHECK(LENGTH(nome) >= 3), -- Validar tamanho do nome é maior ou igual que 3 caracteres
@@ -54,8 +54,8 @@ CREATE TABLE IF NOT EXISTS pessoaFisica (
 
 CREATE TABLE IF NOT EXISTS contrato (
     idContrato INT AUTO_INCREMENT,
-    tipo ENUM('Locacao', 'Evento') NOT NULL DEFAULT ('Locacao'), -- Tipo so pode ser armazenado com 1 desses valores
-    valor_total DECIMAL(5,2) NOT NULL DEFAULT(0),
+    tipo ENUM('Locacao', 'Evento') NOT NULL DEFAULT 'Locacao', -- Tipo so pode ser armazenado com 1 desses valores
+    valor_total DECIMAL(5,2) NOT NULL DEFAULT 0,
     cep VARCHAR(8) NOT NULL, -- Validador para se o cep tem exatamente 8 digitos
     localEvento VARCHAR(150) NOT NULL,
     localRetirada VARCHAR(150) NOT NULL,
@@ -65,10 +65,10 @@ CREATE TABLE IF NOT EXISTS contrato (
     idUsuario INT NOT NULL,
     idContratante INT NOT NULL,
     PRIMARY KEY(idContrato),
-    FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
+    FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
-    FOREIGN KEY (idContratante) REFERENCES Contratante(idContratante)
+    FOREIGN KEY (idContratante) REFERENCES contratante(idContratante)
     ON UPDATE CASCADE
     ON DELETE CASCADE
 );
@@ -77,7 +77,7 @@ CREATE TABLE grupo (
 	idGrupo INT AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     categoria VARCHAR(100) NOT NULL,
-    pregoGrupo DECIMAL(5,2),
+    precoGrupo DECIMAL(10,2),
 	CHECK(LENGTH(nome) >= 3), -- Validar tamanho do nome é maior ou igual que 3 caracteres
     PRIMARY KEY (idGrupo)
 );
@@ -100,7 +100,6 @@ CREATE TABLE IF NOT EXISTS itens (
 	idItens INT AUTO_INCREMENT,
     codBarras VARCHAR(13) NOT NULL, -- Validar se o codigo de barras é maior que 12 BACK-END
     dataLocacao DATETIME NOT NULL,
-    precoItem DECIMAL(10,2) NOT NULL,
 	imagem LONGBLOB,
     idGrupo INT NOT NULL,
     CHECK(LENGTH(codBarras) >= 12), -- Validar se o codigo de barras é maior que 12
