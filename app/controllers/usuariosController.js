33aaa3c9b64fd085301171a458bfb578a8edaba1
LuanContini <luanhenricoa@gmail.com>
2025-01-13 18:10:37 -0300

    .

--- app/controllers/usuariosController.js
+++ app/controllers/usuariosController.js
@@ -1,10 +1,18 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const dbConnection = require("../../config/dbConnection");
-const { getUsuarios, adicionarUsuario, updateUsuario, deleteUsuario, findUsuario } = require("../models/usuariosModel");
-require("dotenv").config({ path: ".env" });
+const {
+  getUsuarios,
+  adicionarUsuario,
+  updateUsuario,
+  deleteUsuario,
+  findUsuario,
+} = require("../models/usuariosModel");
 
-const { SecretsManagerClient, GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager");
+const {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} = require("@aws-sdk/client-secrets-manager");
 
 const secret_name = "Projeto-extensao-4Semestre/.env";
 
@@ -29,8 +37,6 @@ async function getSecret() {
   }
 }
 
-const secret = await getSecret();
-
 module.exports.getUsuarios = async (req, res) => {
   const dbConn = dbConnection();
   try {
@@ -38,7 +44,7 @@ module.exports.getUsuarios = async (req, res) => {
     res.render("./tela_usuario/tela_usuarios.ejs", {
       usuarios: usuarios,
       selectedClient: "",
-      usuario: req.user
+      usuario: req.user,
     });
   } catch (err) {
     res.status(400).send({ "err": err.message });
@@ -50,7 +56,9 @@ module.exports.getUsuarioById = async (req, res) => {
   try {
     const idUsuario = req.params.id;
     const usuarios = await getUsuarios(dbConn);
-    const usuarioById = usuarios.find(usuario => usuario.idUsuario == idUsuario);
+    const usuarioById = usuarios.find(
+      (usuario) => usuario.idUsuario == idUsuario
+    );
     res.status(200).send({ "Usuario": usuarioById });
   } catch (err) {
     res.status(400).send({ "err": err.message });
@@ -76,7 +84,9 @@ module.exports.postUsuario = async (req, res) => {
       tipo
     );
 
-    res.status(200).send({ message: "Usuário adicionado com sucesso!", usuario });
+    res
+      .status(200)
+      .send({ message: "Usuário adicionado com sucesso!", usuario });
   } catch (err) {
     res.status(400).send({ err: err.message });
   }
@@ -85,6 +95,8 @@ module.exports.postUsuario = async (req, res) => {
 module.exports.putUsuario = async (req, res) => {
   const { nome, cpf, telefone, email, senha, dataNasc, tipo } = req.body;
   const idUsuario = req.params.idUsuario;
+
+  const secret = await getSecret();
 
   try {
     const dbConn = dbConnection();
@@ -107,7 +119,9 @@ module.exports.putUsuario = async (req, res) => {
       tipo
     );
 
-    res.status(200).send({ message: "Usuário atualizado com sucesso!", usuario });
+    res
+      .status(200)
+      .send({ message: "Usuário atualizado com sucesso!", usuario });
   } catch (err) {
     res.status(400).send({ err: err.message });
   }
@@ -116,15 +130,16 @@ module.exports.putUsuario = async (req, res) => {
 module.exports.telaPerfil = async (req, res) => {
   const dbConn = dbConnection();
   try {
-    
     const idUsuario = req.params.idUsuario;
 
     console.log(req.user);
-    
+
     const usuarios = await getUsuarios(dbConn);
-    const usuarioById = usuarios.find(usuario => usuario.idUsuario == idUsuario);
+    const usuarioById = usuarios.find(
+      (usuario) => usuario.idUsuario == idUsuario
+    );
     res.render("./tela_perfil/tela_perfil.ejs", {
-      usuario: usuarioById
+      usuario: usuarioById,
     });
   } catch (err) {
     res.status(400).send({ "err": err.message });
@@ -134,37 +149,43 @@ module.exports.telaPerfil = async (req, res) => {
 module.exports.login = async (req, res) => {
   const { nome, senha } = req.body;
 
+  const secret = await getSecret();
+
   try {
     const dbConn = dbConnection();
     const usuario = await findUsuario(dbConn, nome, senha);
 
     if (!usuario) {
-      return res.render('telas_logins/tela_login', {error: "Usuario ou senha não encontrado"});
+      return res.render("telas_logins/tela_login", {
+        error: "Usuario ou senha não encontrado",
+      });
     }
 
     const token = jwt.sign(
       { idUsuario: usuario.idUsuario, nome: usuario.nome, tipo: usuario.tipo },
-      process.env.JWT_SECRET,
-      { expiresIn: '1d' }
+      secret.JWT_SECRET,
+      { expiresIn: "1d" }
     );
 
     req.session.token = token;
 
-    const returnTo = req.session.returnTo || '/itens';
+    const returnTo = req.session.returnTo || "/itens";
     delete req.session.returnTo;
     res.redirect(returnTo);
   } catch (err) {
-      return res.render('telas_logins/tela_login', {error: "Usuario ou senha não encontrado"});
+    return res.render("telas_logins/tela_login", {
+      error: "Usuario ou senha não encontrado",
+    });
   }
 };
 
 module.exports.logout = (req, res) => {
-  req.session.destroy(err => {
+  req.session.destroy((err) => {
     if (err) {
-      return res.status(500).send({ message: 'Erro ao encerrar a sessão.' });
+      return res.status(500).send({ message: "Erro ao encerrar a sessão." });
     }
-    res.clearCookie('connect.sid'); // Limpa o cookie de sessão
-    res.status(200).send({ message: 'Logout realizado com sucesso.' });
+    res.clearCookie("connect.sid"); // Limpa o cookie de sessão
+    res.status(200).send({ message: "Logout realizado com sucesso." });
   });
 };
 
