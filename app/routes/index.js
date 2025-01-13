5a8a4afeaf956f00ea010bc65c2e1259831fbcba
LuanContini <luanhenricoa@gmail.com>
2025-01-13 18:53:19 -0300

    .

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,13 +5,11 @@ const router = express.Router();
 
 const itemRoute = require("./item.routes");
 const clienteRoute = require("./cliente.routes");
-const contratoRoute = require("./contrato.routes");
 const manutencaoRoute = require("./manutencao.routes");
 const usuarioRoute = require("./usuario.routes");
 
 router.use("/itens", auth.checarAuthComum, itemRoute);
 router.use("/clientes", auth.checarAuthComum, clienteRoute);
-router.use("/contrato", auth.checarAuthComum, contratoRoute);
 router.use("/manutencao", auth.checarAuthComum, manutencaoRoute);
 router.use("/usuario", usuarioRoute);
 
